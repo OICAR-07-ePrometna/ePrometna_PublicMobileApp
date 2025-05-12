bcb5eb7f020bc3d39bf058ef92ff295c41fe199d
Matija Lukanić <129276153+mlukanic@users.noreply.github.com>
2025-05-12 12:33:47 +0200

    Update app/login.tsx
    renaming PingTestScreen to LoginScreen
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/login.tsx
+++ app/login.tsx
@@ -2,9 +2,9 @@ import { View } from 'react-native';
 import React from 'react';
 import LoginView from '@/components/LoginView';
 
-export default function PingTestScreen() {
+export default function LoginScreen() {
   return (
-    <View style={{ flex: 1 }} accessibilityLabel="Login Test Screen">
+    <View style={{ flex: 1 }} accessibilityLabel="Login Screen">
       <LoginView />
     </View>
   );
